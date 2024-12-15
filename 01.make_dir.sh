e8803b13118ef345b9c244c45b26d9ba0402a1bc
asaberui1 <739401566@qq.com>
2024-12-15 16:01:48 +0800

    Update 01.make_dir.sh
    Allow directly passing arguments.

--- 01.make_dir.sh
+++ 01.make_dir.sh
@@ -1,20 +1,83 @@
 #!/bin/bash
-#PBS -N 01.wrf-fnl-2022
-#PBS -o wrf.01.o
-#PBS -e wrf.01.e
-#PBS -l walltime=24:00:00
-#PBS -l nodes=1:ppn=1
+# Usage: bash script.sh --month=01:12 --year=xxxx
 
-#cd $PBS_O_WORKDIR
-#for mm in 02
-#for mm in 04 06 09 11
-#for mm in 01 03 05 07 08 10 12
-for mm in 11
+# Parse command line arguments
+for arg in "$@"
 do
-#for d in 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28
-for d in 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30
-#for d in 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31
-do
-  ./wrf_setup -d ${mm}_${d}
+    case $arg in
+        --month=*)
+        months="${arg#*=}"
+        if [[ $months == *":"* ]]; then
+            IFS=":" read -r start_month end_month <<< "$months"
+        else
+            start_month=$months
+            end_month=$months
+        fi
+        ;;
+        --year=*)
+        year="${arg#*=}"
+        ;;
+    esac
 done
+
+# Check if month argument is provided
+if [ -z "$months" ]; then
+    echo "Please provide month(s) using --month=03(:10)"
+    exit 1
+fi
+
+# Check if year argument is provided
+if [ -z "$year" ]; then
+    echo "Please provide a year using --year=xxxx"
+    exit 1
+fi
+
+# Function to determine the number of days in a month
+days_in_month() {
+    month=$1
+    year=$2
+    cal "$month" "$year" | awk 'NF {DAYS = $NF}; END {print DAYS}'
+}
+
+# Determine if the year is a leap year
+is_leap_year() {
+    year=$1
+    if (( (year % 400 == 0) || (year % 4 == 0 && year % 100 != 0) )); then
+        return 0
+    else
+        return 1
+    fi
+}
+
+# Iterate over the specified months
+for ((m=start_month; m<=end_month; m++))
+do
+    month=$(printf "%02d" $m)
+    days=$(days_in_month "$month" "$year")
+    
+    if [ "$m" -eq "$start_month" ] && [ "$m" -eq "$end_month" ]; then
+        for ((d=1; d<=$days; d++))
+        do
+            day=$(printf "%02d" $d)
+            ./wrf_setup -d "${month}_${day}"
+        done
+    elif [ "$m" -eq "$start_month" ]; then
+        for ((d=1; d<=$days; d++))
+        do
+            day=$(printf "%02d" $d)
+            ./wrf_setup -d "${month}_${day}"
+        done
+    elif [ "$m" -eq "$end_month" ]; then
+        for ((d=1; d<=$days; d++))
+        do
+            day=$(printf "%02d" $d)
+            ./wrf_setup -d "${month}_${day}"
+        done
+    else
+        for ((d=1; d<=$days; d++))
+        do
+            day=$(printf "%02d" $d)
+            ./wrf_setup -d "${month}_${day}"
+        done
+    fi
 done
