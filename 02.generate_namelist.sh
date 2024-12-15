41858f6023c480809f4e638adde6842b06e625e4
asaberui1 <739401566@qq.com>
2024-12-15 16:21:44 +0800

    Update 02.generate_namelist.sh
    Allow directly pass arguments.

--- 02.generate_namelist.sh
+++ 02.generate_namelist.sh
@@ -1,36 +1,55 @@
 #!/bin/bash
-#PBS -N 02.wrf-fnl-2022
-#PBS -o wrf.02.o
-#PBS -e wrf.02.e
-#PBS -l walltime=24:00:00
-#PBS -l nodes=1:ppn=1
+# Usage: bash script.sh --month=01(:12) --year=xxxx
 
-#cd $PBS_O_WORKDIR
-
-year=2016
-#for month in 02
-#for month in 04 06 09 11
-#for month in 01 03 05 07 08 10 12
-for month in 11
+# Parse command line arguments
+for arg in "$@"
 do
-#for day in 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28
-for day in 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30
-#for day in 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31
+    case $arg in
+        --month=*)
+        months="${arg#*=}"
+        if [[ $months == *":"* ]]; then
+            IFS=":" read -r start_month end_month <<< "$months"
+        else
+            start_month=$months
+            end_month=$months
+        fi
+        ;;
+        --year=*)
+        year="${arg#*=}"
+        ;;
+    esac
+done
 
+# Check if month argument is provided
+if [ -z "$months" ]; then
+    echo "Please provide month(s) using --month=03(:10)"
+    exit 1
+fi
+
+# Check if year argument is provided
+if [ -z "$year" ]; then
+    echo "Please provide a year using --year=xxxx"
+    exit 1
+fi
+
+for (( month=start_month; month<=end_month; month++ ))
 do
-  ds=$( date -d "${year}/${month}/${day} -6hours" +%d )
-  ms=$( date -d "${year}/${month}/${day} -6hours" +%m )
-  de=$( date -d "${year}/${month}/${day} +1day" +%d )
-  me=$( date -d "${year}/${month}/${day} +1day" +%m )
-  ye=$( date -d "${year}/${month}/${day} +1day" +%Y )
-  echo "${ms}/${ds}" "${me}/${de}"
-  cp namelist.template namelist.input 
-  sed -i "s/YYYY/${year}/g" namelist.input 
-  sed -i "s/YYYE/${ye}/g" namelist.input 
-  sed -i "s/DS/${ds}/g" namelist.input 
-  sed -i "s/MS/${ms}/g" namelist.input 
-  sed -i "s/DE/${de}/g" namelist.input 
-  sed -i "s/ME/${me}/g" namelist.input  
-  mv namelist.input ${month}_${day}/namelist.input
-done 
+    m=$(printf "%02d" $month)
+    for day in {01..31}
+    do
+        ds=$(date -d "${year}/${m}/${day} -6hours" +%d)
+        ms=$(date -d "${year}/${m}/${day} -6hours" +%m)
+        de=$(date -d "${year}/${m}/${day} +1day" +%d)
+        me=$(date -d "${year}/${m}/${day} +1day" +%m)
+        ye=$(date -d "${year}/${m}/${day} +1day" +%Y)
+        echo "${ms}/${ds}" "${me}/${de}"
+        cp namelist.template namelist.input
+        sed -i "s/YYYY/${year}/g" namelist.input
+        sed -i "s/YYYE/${ye}/g" namelist.input
+        sed -i "s/DS/${ds}/g" namelist.input
+        sed -i "s/MS/${ms}/g" namelist.input
+        sed -i "s/DE/${de}/g" namelist.input
+        sed -i "s/ME/${me}/g" namelist.input
+        mv namelist.input ${m}_${day}/namelist.input
+    done
 done
